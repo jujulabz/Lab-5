20155e14410661cc7592730d994b13d2c3097713
jujulabz <mp678503@gmail.com>
2026-02-15 23:58:21 -0500

    finished elif statement
    I messed up the first time A bit. I confused my variables die 1 and die 2 so they were repeating. After troubleshooting the issue everything is working as intended.

--- Lab5_mpalmer33-#1.py
+++ Lab5_mpalmer33-#1.py
@@ -23,4 +23,25 @@ elif(die1 == 1 and die2 == 2) or (die1 == 2 and die2 == 1 ):
     print("Ace Caught a Deuce")
 elif die1 == 2 and die2 == 2:
     print("Little Joe from Kokomo")
-
+elif(die1 == 1 and die2 == 4) or (die1 == 4 and die2 == 1):
+    print(" Little Phoebe")
+elif(die1 == 2 and die2 == 3) or (die1 == 3 and die2 == 2):
+    print("Little Phoebe")
+elif die1 == 3 and die2 == 3:
+    print("Jimmy Hicks from the Sticks")
+elif(die1 == 6 and die2 == 1) or (die1 == 1 and die2 == 6):
+    print("Six Ace")
+elif die1 == 4 and die2 == 4:
+    print("Eighter from Decatur")
+elif (die1 == 3 and die2 == 6) or (die1 == 6 and die2 == 3):
+    print("Nina from Pasadena")
+elif(die1 ==4 and die2 == 5) or (die1 == 5 and die2 == 4):
+    print("Nina from Pasadena")
+elif die1 == 5 and die2 == 5:
+    print("Puppy Paws")
+elif(die1 == 6 and die2 == 5) or (die1 == 5 and die2 == 6):
+    print("Six Five no Jive")
+elif die1 == 6 and die2 == 6:
+    print("Boxcars")
+else:
+    print("Nothing")
