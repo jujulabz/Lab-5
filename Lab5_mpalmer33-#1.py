1ff48e1e4da74a9ac2ffaa8d98ef3c84acf7dec5
jujulabz <mp678503@gmail.com>
2026-02-15 23:25:49 -0500

    Create Lab5_mpalmer33-#1.py
    Adding comment as well as importing random

---
+++ Lab5_mpalmer33-#1.py
@@ -0,0 +1,11 @@
+'''
+Dice Rolling Terms,
+Melissa Palmer,
+rolls two dice, prints the outcome, and then uses conditional statements 
+to print the appropriate term for that roll based on the table below.,
+2/15/2026.
+
+'''
+
+import random
+
