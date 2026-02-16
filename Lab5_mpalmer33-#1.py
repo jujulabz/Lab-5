88425c4f676dc6d3f9469490f1ccc016cd783300
jujulabz <mp678503@gmail.com>
2026-02-15 23:28:46 -0500

    adding vars and print statements

--- Lab5_mpalmer33-#1.py
+++ Lab5_mpalmer33-#1.py
@@ -9,3 +9,11 @@ to print the appropriate term for that roll based on the table below.,
 
 import random
 
+die1 = random.random(1,6)
+die2 = random.random(1,6)
+total = die1 + die2
+
+print("Die 1:", die1)
+print("Die 2:", die2)
+print("Total:",total)
+
